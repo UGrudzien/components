d76d33e3868f51c7b96e2f13add7bb335ab9246f
student19 <student19@pcoip.ki.agh.edu.pl>
2018-04-07 12:16:42 +0200

    spring

---
+++ Spring/src/com/mkyong/common/IPrinter.java
@@ -0,0 +1,7 @@
+package com.mkyong.common;
+
+public interface IPrinter {
+	public void printHello() {
+		System.out.println("");
+}
+}
---
+++ Spring/src/com/mkyong/common/PrintMessage.java
@@ -0,0 +1,13 @@
+package com.mkyong.common;
+
+public class PrintMessage {
+	private String message;
+	
+	public PrintMessage(String message) {
+		this.message=message;
+	}
+	public void printMessage(String message) {
+		
+		System.out.println(message);
+	}
+}
